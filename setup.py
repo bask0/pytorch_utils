1290caf0d259dd1e473f8e678c231c1defbc9236
Basil Kraft <bkraft@bgc-jena.mpg.de>
2021-05-18 13:16:12 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pytorch_utils",
+    version="0.0.0",
+    author="Basil Kraft",
+    author_email="bkraft.work@gmail.com",
+    description=("Utitlities for building PyTorch models and pipelines."),
+    license="MIT",
+    url="https://github.com/bask0/pytorch_utils",
+    long_description=read('README.md'),
+    classifiers=[
+        "Topic :: Utilities",
+        "License :: MIT",
+    ],
+)
